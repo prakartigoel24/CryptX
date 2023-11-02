27817b7d90a303024dd5c07a3dc8902a177fc999
Prakarti Goel <prakartigoel24@gmail.com>
2023-11-03 02:02:42 +0530

    Created the Loader Component

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,20 +1,28 @@
-import React from "react"
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { baseURL } from "../main"
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { baseURL } from "../main";
+import Loader from "./Loader";
 
 const Exchanges = () => {
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchExchanges = async ()=>{
-        const {data} = await axios.get(`${baseURL}/exchanges`);
-        console.log(data);
-    }
+    const fetchExchanges = async () => {
+      const { data } = await axios.get(`${baseURL}/exchanges`);
+      // setLoading(false);
+      console.log(data);
+    };
     fetchExchanges();
-  },[])
-  return (
-    <div>Exchanges</div>
-  )
-}
+  }, []);
 
-export default Exchanges
+  return <div>
+    {loading ? <Loader /> : 
+    <>
+    data fetched
+    </>
+    }
+    </div>;
+};
+
+export default Exchanges;
