ac7aa31569747fb1a49e542d2c0294cbf44e3862
Prakarti Goel <prakartigoel24@gmail.com>
2023-11-03 00:56:55 +0530

    Created basic navbar

---
+++ src/components/Branding.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Branding = () => {
+  return (
+    <div className='flex items-center justify-start'>
+        <img src="logo.png" alt="Logo-Here" height={10} width={70}/>
+        <strong className='text-xl'>CryptX</strong>
+    </div>
+  )
+}
+
+export default Branding
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Branding from "./Branding";
 
 const Navbar = () => {
   return (
-    <div>
-        <Link to = "/">Home</Link>
-        <Link to = "/about-developer">Developer</Link>
-        <Link to = "/">Coins</Link>
-        <Link to = "/">Exchanges</Link>
+    <div className="bg-black text-white sticky flex justify-between items-center mx-auto px-5">
+      <Branding/>
+      <div className="flex gap-4">
+        <Link to="/" className="hover:text-gray-400">Home</Link>
+        <Link to="/coins" className="hover:text-gray-400">Coins</Link>
+        <Link to="/exchanges" className="hover:text-gray-400">Exchanges</Link>
+        <Link to="/about-developer" className="hover:text-gray-400">Developer</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
