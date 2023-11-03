ef0b35bdfd0985b4edea5821b93b374fa58d8b16
Prakarti Goel <prakartigoel24@gmail.com>
2023-11-03 12:11:47 +0530

    Added Exchanges page

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -3,26 +3,42 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { baseURL } from "../main";
 import Loader from "./Loader";
+import ExchangeCard from "./ExchangeCard";
 
 const Exchanges = () => {
   const [loading, setLoading] = useState(true);
+  const [exchanges, setExchanges] = useState([]);
 
   useEffect(() => {
     const fetchExchanges = async () => {
       const { data } = await axios.get(`${baseURL}/exchanges`);
-      // setLoading(false);
-      console.log(data);
+      setLoading(false);
+      setExchanges(data);
     };
     fetchExchanges();
   }, []);
 
-  return <div>
-    {loading ? <Loader /> : 
+  return (
     <>
-    data fetched
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-wrap items-center justify-center w-full my-6">
+          {exchanges.map((curr_item, id) => {
+            return (
+              <ExchangeCard
+                key={curr_item.id}
+                name={curr_item.name}
+                img={curr_item.image}
+                rank={curr_item.trust_score_rank}
+                url={curr_item.url}
+              />
+            );
+          })}
+        </div>
+      )}
     </>
-    }
-    </div>;
+  );
 };
 
 export default Exchanges;
